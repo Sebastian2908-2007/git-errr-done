cef4e0e8752ebbf3b43bb24cc76f30ce996f76aa
Sebabin <sebastiancrespin279@gmail.com>
2021-11-25 13:08:43 -0700

    have user repos and issues dynamically generating(Happy Thanksgiving!)

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,9 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
 var getUserRepos = function(user) {
 // format the github api url
 apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -5,9 +11,62 @@ apiUrl = "https://api.github.com/users/" + user + "/repos";
 // make a request to the url
 fetch(apiUrl).then(function(response){
 response.json().then(function(data) {
-    console.log(data);
+    //console.log(data);
+ 
+
+      displayRepos(data, user);
+    
    });
  });
 };
 
-getUserRepos();
+var displayRepos = function(repos, searchTerm) {
+  repoContainerEl.textContent = "";
+  repoSearchTerm.textContent = searchTerm;
+  // loop over repos
+  for (i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+    // create a container for each repo
+    var repoEl = document.createElement('div');
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    // create a span element to hold repo name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+
+    // append to container
+    repoEl.appendChild(titleEl);
+    // create a status element
+    var statusEl = document.createElement('span');
+    statusEl.classList = "flex-row align-center";
+
+    // check if curent repo has issues or not
+    if(repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+       "<i class='fas fa-times status-icon icon-danger'></i>"  + " "  + repos[i].open_issues_count +  " " +  "issues(s)";
+    }else {
+      statusEl.innerHTML = "<i class= 'fas fa-check-square status-icon icon-success'></i>";
+    }
+//  append statusEl to the repoEl container
+repoEl.appendChild(statusEl);
+
+    // append container to dom
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+ var username = nameInputEl.value.trim();
+
+ if(username) {
+   getUserRepos(username);
+   nameInputEl.value = "";
+ }else {
+   alert("please enter a GitHub username");
+ }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
